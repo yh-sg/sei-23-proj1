1c34830cb0bb5bf50562a51ed82d6f2eb80f4982
yonghan <wangyh.92@gmail.com>
2020-06-26 16:36:35 +0800

    basics functions with animations

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-document.querySelector(".yellow").addEventListener("click", function name() {
-    alert("Clicked")    
-})
+//All global variables
+let chosenColorArray = [];
+let randomColorArray = [];
+let randomColor;
+let arrayButtonColor = ["yellow", "blue", "red", "green"];
+
+//All DOM Manipulation
+let buttons = document.querySelectorAll(".btn");
+
+//when clicked, get the color and add into array
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function name() {
+    let chosenColor = this.getAttribute("id");
+    let x = chosenColorArray.push(chosenColor);
+
+    pressAnimation(chosenColor);
+
+    console.log(chosenColorArray);
+  });
+}
+
+nextRandomColor();
+
+//Pick a random color and save the color pattern
+function nextRandomColor() {
+  let randomColor = Math.floor(Math.random() * 4);
+  randomColor = arrayButtonColor[randomColor];
+  randomColorArray.push(randomColor);
+
+  pressAnimation(randomColor);
+}
+
+//pressanimation
+function pressAnimation(chosenColor){
+    document.querySelector("#" + chosenColor).classList.add("pressedAnimation");
+    setTimeout(function () {
+      document.querySelector("#" + chosenColor).classList.remove("pressedAnimation");
+    },200)
+}
